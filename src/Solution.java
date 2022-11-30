6116dc91910fce0b643fc853c902e536aee4fc3b
Misha Lytovchenko <alex.elmagnifiko.98@gmail.com>
2022-11-30 22:08:54 +0200

    Commit

--- src/Solution.java
+++ src/Solution.java
@@ -11,7 +11,7 @@ public class Solution {
         System.out.println(Arrays.toString(file.listFiles()));
         for(File s : file.listFiles()){
             if(s.isDirectory()){
-                System.out.println(s.getName());
+                System.out.println(s.getName() + "Mishanya");
             }
         }
         for(File s : file.listFiles()) {
